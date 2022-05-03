95e1c55901520adefa087df54b2aa6c3c8a7a5fd
Konstantin <kastielm368@gmail.com>
2022-05-03 05:35:12 +0300

    new]

--- app.js
+++ app.js
@@ -10,4 +10,24 @@ function getAge() {
 let output = `Hi, my name is ${nameTest} and my age is ${getAge() > 20 ? 'Pezduk' : 'Maybe'} years`;
 console.log(output);
 
-console.log(nameTest.trim());
+let peoples = [
+  { name: 'Vladilen', budget: 4200 },
+  { name: 'Elena', budget: 3500 },
+  {name: 'Victoria', budget:1700}
+];
+
+let indexBudgetIndex = peoples.findIndex(function(person) {
+  return person.budget === 3500;
+})
+console.log(indexBudgetIndex);
+
+let indexBudgetPerson = peoples.find(function(person) {
+  return person.budget === 3500;
+})
+let personBudgetNameArrowWay = peoples.find((person) => person.budget === 3500);
+console.log(personBudgetNameArrowWay.name);
+let cars = ['Mazda','Mersedes'];
+console.log(cars.includes('Mazda'));
+
+let upperCaseCars = cars.map(car => car.toUpperCase());
+console.log(upperCaseCars);
