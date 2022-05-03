b396f2f8b6b25a3ec8730bd6766af9eeb0151e99
Konstantin <kastielm368@gmail.com>
2022-05-03 04:01:58 +0300

    new

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log('Stroki bleat');
 
+let nameTest = 'Kostyan';
+let ageTest = 29;
+
+function getAge() {
+  return ageTest;
+}
+
+let output = `Hi, my name is ${nameTest} and my age is ${getAge() > 20 ? 'Pezduk' : 'Maybe'} years`;
+console.log(output);
+
+console.log(nameTest.trim());
